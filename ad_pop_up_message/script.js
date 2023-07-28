c28c218dea22017bb316223e32deb8b311119fb3
Bhushan <105504134+Itssagarcode@users.noreply.github.com>
2023-07-28 23:32:16 +0530

    Add files via upload

---
+++ ad_pop_up_message/script.js
@@ -0,0 +1,11 @@
+let modelBox = document.getElementById("modelbox")
+document.onmouseleave =function(){
+    modelBox.style.display ="block";
+}
+
+
+
+
+  function closeModel(){
+    modelBox.style.display = "none"
+  } 
